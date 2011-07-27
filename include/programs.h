7e75b43e1ce8da632c8e68da7a71c3953177aaf0
Merritt Boyd <mboyd@mit.edu>
2011-07-27 12:15:54 -0400

    Let's try that one more time...

--- include/programs.h
+++ include/programs.h
@@ -1,4 +1,4 @@
-char divide_op[] = "!!ARBfp1.0\n"
+char divide_op_old[] = "!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -12,7 +12,22 @@ char divide_op[] = "!!ARBfp1.0\n"
 "END\n"
 "# 4 instructions, 1 R-regs, 0 H-regs\n";
 
-char copy_op[] = "!!ARBfp1.0\n"
+char divide_op[] = "
+
+varying vec2 texcoord0;
+uniform sampler2D texture0;
+
+void main(void) {
+  
+  vec4 t1 = texture2D(texture0, texcoord0);
+  vec4 t2 = texture2D(texture0, gl_FragCoord);
+
+  gl_FragColor = vec4(t2.x / t1.x, 0, 0, 0);
+}
+
+";
+
+char copy_op_old[] = "!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP RC;\n"
@@ -21,8 +36,19 @@ char copy_op[] = "!!ARBfp1.0\n"
 "TEX   oCol.x, fragment.position, texture[0], RECT;\n"
 "END\n";
 
+char copy_op[] = "
 
-char swaprow_op[] = "!!ARBfp1.0\n"
+varying vec2 texcoord0;
+uniform sampler2D texture0;
+
+void main(void) {
+  gl_FragColor = texture2D(texture0, texcoord0);
+}
+
+";
+
+
+char swaprow_op_old[] = "!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -34,8 +60,23 @@ char swaprow_op[] = "!!ARBfp1.0\n"
 "TEX   oCol.x, R0, texture[0], RECT;\n"
 "END\n";
 
+char swaprow_op[] = "
 
-char swapcol_op[]="!!ARBfp1.0\n"
+varying vec2 texcoord0;
+uniform sampler2D texture0;
+
+uniform float y;
+
+void main(void) {
+  vec2 coord = texcoord0;
+  coord.y = y;
+  gl_FragColor = texture2D(texture0, texcoord0);
+}
+
+";
+
+
+char swapcol_op_old[]="!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -47,7 +88,22 @@ char swapcol_op[]="!!ARBfp1.0\n"
 "TEX   oCol.x, R0, texture[0], RECT;\n"
 "END\n";
 
-char quadtree_op[]="!!ARBfp1.0\n"
+char swapcol_op[] = "
+
+varying vec2 texcoord0;
+uniform sampler2D texture0;
+
+uniform float y;
+
+void main(void) {
+  vec2 coord = texcoord0;
+  coord.y = y;
+  gl_FragColor = texture2D(texture0, texcoord0);
+}
+
+";
+
+char quadtree_op_old[]="!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -62,8 +118,25 @@ char quadtree_op[]="!!ARBfp1.0\n"
 "MOVR  oCol.x, |R0|;\n"
 "END\n";
 
+char quadtree_op[] = "
 
-char row_op[] = "!!ARBfp1.0\n"
+varying vec2 texcord0;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+
+uniform vec4 c;
+
+void main(void) {
+  vec4 r0 = texture2D(texture1, gl_FragCoord);
+  vec4 r1 = texture2D(texture0, c);
+
+  
+}
+
+";
+
+
+char row_op_old[] = "!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -78,8 +151,24 @@ char row_op[] = "!!ARBfp1.0\n"
 "MADR  oCol, -R0.x, R1.x, R2.x;\n"
 "END\n";
 
+char row_op[] = "
 
-char max_op[] = "!!ARBfp1.0\n"
+varying vec2 texcoord0;
+varying vec2 texcoord1;
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+
+void main(void) {
+  vec4 r0 = texture2D(texture0, texcoord0);
+  vec4 r2 = texture2D(texture0, gl_FragCoord);
+  vec4 r1 = texture2D(texture0, texcoord1);
+  gl_FragColor = r1.x + r2.x - r0.x;
+}
+
+";
+
+
+char max_op_old[] = "!!ARBfp1.0\n"
 "OPTION NV_fragment_program2;\n"
 "PARAM c[1] = { program.local[0] };\n"
 "TEMP R0;\n"
@@ -91,3 +180,18 @@ char max_op[] = "!!ARBfp1.0\n"
 "TEX   R0.x, fragment.position, texture[0], RECT;\n"
 "MAXR  oCol.x, |R0|, |R1|;\n"
 "END\n";
+//FIXME: are those abs's?
+char max_op[] = "
+
+varying vec2 texcoord0;
+uniform sampler2D texture0;
+
+void main(void) {
+  vec4 r1 = texture2D(texcoord0, texture0);
+  vec4 r0 = texture2D(gl_FragCoord, texture0);
+  gl_FragColor = max(r0, r1);
+}
+
+";
+
+
